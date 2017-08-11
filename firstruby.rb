961a364033cb34fa056d753e08561fc1d8bd34b4
ManasaDornala <manasa.dornala@appsassociates.com>
2017-08-11 00:52:08 -0400

    Update firstruby.rb

--- firstruby.rb
+++ firstruby.rb
@@ -1,1 +1,11 @@
-Class cacheobersver
+class example
+  observe:base_model
+  
+  %w(update create destroy).each { |name|
+    define_method("after_#{name}") {|record| expire_cache_for(record)}}
+  private
+  
+  def expire_cache_for(_record)
+    @cache ||= ActionController:Base.New
+  end
+end
